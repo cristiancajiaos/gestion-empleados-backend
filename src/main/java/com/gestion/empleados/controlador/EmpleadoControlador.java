d355ee90450281976a2eb7bf497f3ef9d6690a4f
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2022-07-02 11:04:48 -0400

    Creado método para guardar empleado

--- src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,15 @@ public class EmpleadoControlador {
 	@Autowired
 	private EmpleadoRepositorio repositorio;
 	
+	/* Listar todos los empleados */
 	@GetMapping("/empleados")
 	public List<Empleado> listarTodosLosEmpleados(){
 		return repositorio.findAll();
+	}
+	
+	/* Guardar empleado */
+	@PostMapping("/empleados")
+	public Empleado guardarEmpleado(@RequestBody Empleado empleado) {
+		return repositorio.save(empleado);
 	}
 }
