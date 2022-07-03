c33af1dae48e7c104792fc4fb0069272e0ddd31c
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2022-07-02 21:52:56 -0400

    Creada consulta API /empleados/id

--- src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -3,13 +3,16 @@ package com.gestion.empleados.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gestion.empleados.excepciones.ResourceNotFoundException;
 import com.gestion.empleados.modelo.Empleado;
 import com.gestion.empleados.repositorio.EmpleadoRepositorio;
 
@@ -31,5 +34,12 @@ public class EmpleadoControlador {
 	@PostMapping("/empleados")
 	public Empleado guardarEmpleado(@RequestBody Empleado empleado) {
 		return repositorio.save(empleado);
+	}
+	
+	/* Buscar y modificar empleado */
+	@GetMapping("/empleados/{id}")
+	public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Long id) {
+		Empleado empleado = repositorio.findById(id).orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el ID " + id));
+		return ResponseEntity.ok(empleado);
 	}
 }
